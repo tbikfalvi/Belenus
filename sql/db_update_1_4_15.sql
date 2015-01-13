8df05fbe1a49418c624c31b6cd699f4af6df452c
Tamas Bikfalvi <tbikfalvi@gmail.com>
2015-01-13 22:07:46 +0100

    Csomag

---
+++ sql/db_update_1_4_15.sql
@@ -0,0 +1,15 @@
+-- -----------------------------------------------------------------------------------
+-- Belenus Szoftver Rendszer (c) Pagony Multimedia Studio Bt - 2014
+-- -----------------------------------------------------------------------------------
+-- Filename    : db_update_1_4_15.sql
+-- AppVersion  : 1.4.15
+-- DbVersion   : 1.6.1
+-- -----------------------------------------------------------------------------------
+
+USE `belenus`;
+
+-- -----------------------------------------------------------------------------------
+
+UPDATE settings SET value=NOW() WHERE identifier="GLOBAL_DATA_UPDATED";
+UPDATE settings SET value='1_4_15' WHERE identifier='APPLICATION_VERSION';
+UPDATE settings SET value='1_6_1' WHERE identifier='DATABASE_VERSION';
