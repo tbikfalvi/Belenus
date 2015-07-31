550cf39c277d0eeacbeca88558bb295e18d26113
Tamas Bikfalvi <tbikfalvi@gmail.com>
2015-07-31 12:38:23 +0200

    1_5_2 kesz talan

--- sql/db_fill_hu.sql
+++ sql/db_fill_hu.sql
@@ -2,7 +2,7 @@
 -- Belenus Szoftver Rendszer (c) Pagony Multimedia Studio Bt - 2013
 -- -----------------------------------------------------------------------------------
 -- Filename    : db_fill_hu.sql
--- AppVersion  : 1.5.1.5
+-- AppVersion  : 1.5.2
 -- DbVersion   : 1.7.0
 -- Author      : Bikfalvi Tamas
 -- -----------------------------------------------------------------------------------
@@ -3648,7 +3648,7 @@ ALTER TABLE `shoppingcartitems` auto_increment=1;
 INSERT INTO `settings` (`settingId`, `identifier`, `value`) VALUES
  (NULL, 'GLOBAL_DATA_UPDATED', '2015-01-01 08:00:00'),
  (NULL, 'ABOUT_INFO_LINK', 'http://www.kiwisun.eu/elerhetosegeink.html'),
- (NULL, 'APPLICATION_VERSION', '1_5_1_5'),
+ (NULL, 'APPLICATION_VERSION', '1_5_2'),
  (NULL, 'DATABASE_VERSION', '1_7_0');
 
 -- -----------------------------------------------------------------------------------
