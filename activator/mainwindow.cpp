d3acb06ad9c024cb10afe9d8eb33e899f5daa061
Tamas Bikfalvi <tbikfalvi@gmail.com>
2015-10-20 06:35:43 +0200

    Uj London

--- activator/mainwindow.cpp
+++ activator/mainwindow.cpp
@@ -136,7 +136,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
                                      << "BLNS34"
                                      << "BLNS35"
                                      << "BLNS36"
-//                                     << "BLNS37"
+                                     << "BLNS37"
 //                                     << "BLNS38"
 //                                     << "BLNS39"
 //                                     << "BLNS40"
