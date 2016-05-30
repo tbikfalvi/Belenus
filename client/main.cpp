c71c7aabeb5e2e88612b04be2bd96c42a2cec0ea
Tamas Bikfalvi <tbikfalvi@gmail.com>
2016-05-30 18:25:43 +0200

    version

--- client/main.cpp
+++ client/main.cpp
@@ -13,7 +13,7 @@
 // Alkalmazas fo allomanya.
 //====================================================================================
 
-#define APPLICATION_VERSION_NUMBER  "1.5.4.0"
+#define APPLICATION_VERSION_NUMBER  "1.5.4.1"
 #define DATABASE_VERSION_NUMBER     "1.7.1"
 
 //====================================================================================
