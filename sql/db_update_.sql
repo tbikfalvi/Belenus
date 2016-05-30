64fa09290352bd1b85bc4e5b231d12419c5ca8f8
Tamas Bikfalvi <tbikfalvi@gmail.com>
2016-05-30 17:54:56 +0200

    Revert "Install package"
    This reverts commit 50682a4ac165c2fb3a2ae1d332e1f37dbad81ebe.

--- sql/db_update_.sql
+++ sql/db_update_.sql
@@ -1,8 +1,8 @@
 -- -----------------------------------------------------------------------------------
 -- Belenus Szoftver Rendszer (c) Pagony Multimedia Studio Bt - 2014
 -- -----------------------------------------------------------------------------------
--- Filename    : db_update_1_5_4_1.sql
--- AppVersion  : 1.5.4.1
+-- Filename    : db_update_1_5_4_0.sql
+-- AppVersion  : 1.5.4.0
 -- DbVersion   : 1.7.1
 -- -----------------------------------------------------------------------------------
 
@@ -14,5 +14,5 @@ USE `belenus`;
 -- -----------------------------------------------------------------------------------
 
 UPDATE settings SET value=NOW() WHERE identifier="GLOBAL_DATA_UPDATED";
-UPDATE settings SET value='1_5_4_1' WHERE identifier='APPLICATION_VERSION';
+UPDATE settings SET value='1_5_4_0' WHERE identifier='APPLICATION_VERSION';
 UPDATE settings SET value='1_7_1' WHERE identifier='DATABASE_VERSION';
