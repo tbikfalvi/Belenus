9bb72f6b8af5f3637de826e3609be42b261ce523
Tamas Bikfalvi <tbikfalvi@gmail.com>
2018-09-03 07:59:16 +0200

    Forditasok

--- Advertisement/advertisement_ro.ts
+++ Advertisement/advertisement_ro.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ro_MD">
+<TS version="2.1" language="ro_MD">
 <context>
     <name>QObject</name>
     <message>
         <location filename="main.cpp" line="63"/>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation type="finished">Avertisment</translation>
     </message>
     <message>
         <location filename="main.cpp" line="64"/>
@@ -19,7 +19,15 @@ Error code: %1
 4 &gt; An error occurred when attempting to write to the process.
 3 &gt; An error occurred when attempting to read from the process.
 5 &gt; An unknown error occurred.</source>
-        <translation type="unfinished"></translation>
+        <translation type="finished">Eroare la inițializarea procesului:Advertisement.exe
+
+Cod eroare: %1
+0 &gt; Procesul nu a pornit.
+1 &gt; După pornirea cu succes, procesul s-a oprit.
+2 &gt; Ultima funcție așteaptă...() a expirat.
+4 &gt; În timpul încercării de a scrie la proces a intervenit o eroare.
+3 &gt; În timpul încercării de a citi din proces a intervenit o eroare.
+5 &gt; A intervenit o eroare necunoscută.</translation>
     </message>
 </context>
 <context>
@@ -27,32 +35,32 @@ Error code: %1
     <message>
         <location filename="advertisementwindow.ui" line="14"/>
         <source>Advertisement</source>
-        <translation type="unfinished"></translation>
+        <translation type="finished">Publicitate</translation>
     </message>
     <message>
         <location filename="advertisementwindow.ui" line="57"/>
         <source>TextLabel</source>
-        <translation type="unfinished"></translation>
+        <translation type="finished">EtichetăText</translation>
     </message>
     <message>
         <location filename="advertisementwindow.ui" line="125"/>
         <source>Start slideshow</source>
-        <translation type="unfinished"></translation>
+        <translation type="finished">Pornește succesiunea de imagini</translation>
     </message>
     <message>
         <location filename="advertisementwindow.ui" line="150"/>
         <source>Pause slideshow</source>
-        <translation type="unfinished"></translation>
+        <translation type="finished">Oprește succesiunea de imagini</translation>
     </message>
     <message>
         <location filename="advertisementwindow.ui" line="188"/>
         <source>Save window position and size</source>
-        <translation type="unfinished"></translation>
+        <translation type="finished">Salvează poziția și dimensiunile ferestrei</translation>
     </message>
     <message>
         <location filename="advertisementwindow.ui" line="213"/>
         <source>Reload advertisement settings</source>
-        <translation type="unfinished"></translation>
+        <translation type="finished">Reîncarcă setările de publicitate</translation>
     </message>
 </context>
 </TS>
