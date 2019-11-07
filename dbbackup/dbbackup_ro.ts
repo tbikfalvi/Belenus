e45f92dccf0e65efb9862b7eba8a9a33f6fa32b4
Tamas Bikfalvi <tbikfalvi@gmail.com>
2019-11-07 20:49:58 +0100

    1.8.0.2 kesz

--- dbbackup/dbbackup_ro.ts
+++ dbbackup/dbbackup_ro.ts
@@ -9,89 +9,94 @@
         <translation>Copiere de siguranță a bazei de date</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="148"/>
+        <location filename="mainwindow.ui" line="61"/>
+        <source> v. </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="154"/>
         <source>Backup database ...</source>
         <translation>Copiere de siguranță a bazei de date ...</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="155"/>
-        <location filename="mainwindow.cpp" line="302"/>
+        <location filename="mainwindow.ui" line="161"/>
+        <location filename="mainwindow.cpp" line="315"/>
         <source>Please wait until the process finish.</source>
         <translation>Așteaptă finalizarea.</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="180"/>
+        <location filename="mainwindow.ui" line="186"/>
         <source>...</source>
         <translation>...</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="187"/>
+        <location filename="mainwindow.ui" line="193"/>
         <source>Start</source>
         <translation>Start</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="32"/>
+        <location filename="mainwindow.cpp" line="36"/>
         <source>Restore database</source>
         <translation>Restaurarea bazei de date</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="33"/>
-        <location filename="mainwindow.cpp" line="42"/>
+        <location filename="mainwindow.cpp" line="37"/>
+        <location filename="mainwindow.cpp" line="46"/>
         <source>Please select desired database and click on Start</source>
         <translation>Selectează baza de date dorită și dă click pe Start</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="39"/>
+        <location filename="mainwindow.cpp" line="43"/>
         <source>Update database</source>
         <translation>Actualizarea baza de date</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="118"/>
-        <location filename="mainwindow.cpp" line="158"/>
-        <location filename="mainwindow.cpp" line="170"/>
-        <location filename="mainwindow.cpp" line="185"/>
-        <location filename="mainwindow.cpp" line="204"/>
-        <location filename="mainwindow.cpp" line="214"/>
-        <location filename="mainwindow.cpp" line="244"/>
-        <location filename="mainwindow.cpp" line="307"/>
+        <location filename="mainwindow.cpp" line="138"/>
+        <location filename="mainwindow.cpp" line="176"/>
+        <location filename="mainwindow.cpp" line="188"/>
+        <location filename="mainwindow.cpp" line="203"/>
+        <location filename="mainwindow.cpp" line="222"/>
+        <location filename="mainwindow.cpp" line="232"/>
+        <location filename="mainwindow.cpp" line="260"/>
+        <location filename="mainwindow.cpp" line="320"/>
         <source>Error</source>
         <translation>Eroare</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="119"/>
-        <location filename="mainwindow.cpp" line="171"/>
-        <location filename="mainwindow.cpp" line="186"/>
-        <location filename="mainwindow.cpp" line="215"/>
-        <location filename="mainwindow.cpp" line="245"/>
+        <location filename="mainwindow.cpp" line="139"/>
+        <location filename="mainwindow.cpp" line="189"/>
+        <location filename="mainwindow.cpp" line="204"/>
+        <location filename="mainwindow.cpp" line="233"/>
+        <location filename="mainwindow.cpp" line="261"/>
         <source>Unable to execute the following command:
 %1</source>
         <translation>Nu se poate executa următoarea comandă:
 %1</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="127"/>
+        <location filename="mainwindow.cpp" line="147"/>
         <source>Warning</source>
         <translation>Avertisment</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="128"/>
+        <location filename="mainwindow.cpp" line="148"/>
         <source>Location of the MySQL archive application is not set.
 Please check your settings.</source>
         <translation>Locația aplicației de arhivare MySQL nu este setată.
 Verifică setările.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="159"/>
+        <location filename="mainwindow.cpp" line="177"/>
         <source>Unable to create temporary file: dbrecreate.sql</source>
         <translation>Nu se poate crea fișierul temporar: dbrecreate.sql</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="205"/>
+        <location filename="mainwindow.cpp" line="223"/>
         <source>Unable to create temporary file: dbreactivate.sql</source>
         <translation>Nu se poate crea fișierul temporar: dbrecreate.sql</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="308"/>
+        <location filename="mainwindow.cpp" line="321"/>
         <source>The selected file not exists or not readable
 
 %1</source>
