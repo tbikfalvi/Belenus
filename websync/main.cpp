143241244badc302c7021a3ba71e917e7ca2c1d3
Tamas Bikfalvi <tbikfalvi@gmail.com>
2025-03-06 21:45:09 +0100

    Update main.cpp

--- websync/main.cpp
+++ websync/main.cpp
@@ -23,8 +23,6 @@ QTranslator     *poTransQT;
 QApplication    *apMainApp;
 
 //====================================================================================
-#define APPLICATION_VERSION_NUMBER  "2.1.2.0"
-#define DATABASE_VERSION_NUMBER     "2.1.2.0"
 
 #include "../framework/qtlogger.h"
 #include "../framework/qtframework.h"
