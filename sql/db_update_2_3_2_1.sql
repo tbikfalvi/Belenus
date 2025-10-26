00d9760d08bc35297c822a73c42b79f4be9ea9c5
Tamas Bikfalvi <tbikfalvi@gmail.com>
2025-10-26 19:56:00 +0100

    telepito

---
+++ sql/db_update_2_3_2_1.sql
@@ -0,0 +1,19 @@
+﻿-- -----------------------------------------------------------------------------------
+-- Belenus Szoftver Rendszer (c) Bikfalvi Tamas
+-- -----------------------------------------------------------------------------------
+-- Filename    : db_update_2_3_2_1.sql
+-- -----------------------------------------------------------------------------------
+
+USE `belenus`;
+
+-- -----------------------------------------------------------------------------------
+
+ALTER TABLE `report_cardhistory` ADD `barcode` varchar(20) NOT NULL AFTER `userName`;
+
+-- -----------------------------------------------------------------------------------
+
+UPDATE settings SET value='2_3_2_0' WHERE identifier='APPLICATION_VERSION';
+UPDATE settings SET value='2_3_2_0' WHERE identifier='WEBSYNC_VERSION';
+UPDATE settings SET value='2_3_2_1' WHERE identifier='VIEWER_VERSION';
+UPDATE settings SET value='2_3_2_0' WHERE identifier='BACKUP_VERSION';
+UPDATE settings SET value='2_3_2_0' WHERE identifier='DATABASE_VERSION';
