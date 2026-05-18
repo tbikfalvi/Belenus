1438aad81f6754f4f04e7c666ee68ce307a2bc94
Tamas Bikfalvi <tbikfalvi@gmail.com>
2026-05-18 18:04:58 +0200

    telepito

---
+++ sql/db_update_2_3_3_0.sql
@@ -0,0 +1,20 @@
+﻿-- -----------------------------------------------------------------------------------
+-- Belenus Szoftver Rendszer (c) Bikfalvi Tamas
+-- -----------------------------------------------------------------------------------
+-- Filename    : db_update_2_3_3_0.sql
+-- -----------------------------------------------------------------------------------
+
+USE `belenus`;
+
+-- -----------------------------------------------------------------------------------
+
+ALTER TABLE `patientcardtypes` ADD `minimumTime` int(11) NOT NULL DEFAULT 0 AFTER `unitTime`;
+ALTER TABLE `patientcardunits` ADD `minimumTime` int(11) NOT NULL DEFAULT 0 AFTER `unitTime`;
+
+-- -----------------------------------------------------------------------------------
+
+UPDATE settings SET value='2_3_3_0' WHERE identifier='APPLICATION_VERSION';
+UPDATE settings SET value='2_3_3_0' WHERE identifier='WEBSYNC_VERSION';
+UPDATE settings SET value='2_3_3_0' WHERE identifier='VIEWER_VERSION';
+UPDATE settings SET value='2_3_3_0' WHERE identifier='BACKUP_VERSION';
+UPDATE settings SET value='2_3_3_0' WHERE identifier='DATABASE_VERSION';
